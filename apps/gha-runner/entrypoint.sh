fd33675bc79da3e4c4fe29e44a4b776b9fd43f58
Szymon Richert <szymon.richert@consultant.LEGO.com>
2022-12-16 09:05:02 +0100

    Rework gha-runner container:

---
+++ apps/gha-runner/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+registration_url="https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/actions/runners/registration-token"
+echo "Requesting registration URL at '${registration_url}'"
+payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PAT}" ${registration_url})
+export RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
+./config.sh \
+    --name $(hostname) \
+    --token ${RUNNER_TOKEN} \
+    --url https://github.com/${GITHUB_OWNER}/${GITHUB_REPOSITORY} \
+    --work ${RUNNER_WORKDIR} \
+    --unattended \
+    --replace
+
+remove() {
+    ./config.sh remove --token "${RUNNER_TOKEN}"
+}
+
+removeAndWait(){
+    sleep 600
+    remove
+}
+
+
+trap 'removeAndWait; exit 130' INT
+trap 'remove; exit 143' TERM
+./run.sh "$*" &
+wait $!
