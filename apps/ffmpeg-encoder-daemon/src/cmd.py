344eeedb7c7997e0c2d3fb1c1c9786e8235c24fa
Szymon Richert <szymon.richert@consultant.LEGO.com>
2023-02-11 08:53:02 +0100

    Add ffmpeg-encoder-daemon container

--- apps/ffmpeg-encoder-daemon/src/cmd.py
+++ apps/ffmpeg-encoder-daemon/src/cmd.py
@@ -31,10 +31,11 @@ for root,d_names,f_names in os.walk(os.path.abspath(args.root_dir)):
             try:
                 details = ffmpeg_file.load()
                 logging.info(str(details))
-
-                for data in ffmpeg_file.convert(threads=multiprocessing.cpu_count()):
-                    progress = data['progress']
-                    logging.info(f'Conversion progress {progress}%.')
+                
+                if ffmpeg_file.is_hevc:
+                    for data in ffmpeg_file.convert(threads=multiprocessing.cpu_count()):
+                        progress = data['progress']
+                        logging.info(f'Conversion progress {progress}%.')
                 
             except FFMpegFileError as e:
                 logging.info(str(e))
