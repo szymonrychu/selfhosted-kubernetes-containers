2bf8a72eaf32dec346775a0b94b624399e53ce94
Szymon Richert <szymon.rychu@gmail.com>
2023-11-22 21:56:57 +0100

    Update renovate schedule, simplify config

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -17,30 +17,4 @@ module.exports = {
   "onboarding": false,
   "platform": "github",
   "repositories": ["szymonrychu/selfhosted-kubernetes-containers"],
-  "customManagers": [
-    {
-      "customType": "regex",
-      "fileMatch": ["(^|/)Dockerfile$"],
-      "matchStrings": [
-        "#\\s?renovate: image=(?<depName>.*?)\\s?ARG\\s[\\w+\\_]*\\s?=\\s?\\\"?(?<currentValue>[\\w+\\.\\-]*)"
-      ],
-      "datasourceTemplate": "docker"
-    },
-  ],
-  "packageRules": [
-    {
-      "matchManagers": ["regex"],
-      "postUpgradeTasks": {
-        "commands": [
-          "version=$(cat {{{parentDir}}}/VERSION | awk '{print $2}')\n            major=$(echo $version | cut -d. -f1)\n            minor=$(echo $version | cut -d. -f2)\n            patch=$(echo $version | cut -d. -f3)\n            minor=$(expr $minor + 1)\n            echo \"Replacing $version with $major.$minor.$patch\"\n            echo \"$major.$minor.$patch\" > {{{parentDir}}}/VERSION\n            cat {{{parentDir}}}/VERSION\n            "
-        ],
-        "executionMode": "branch"
-      }
-    },
-    {
-      "matchUpdateTypes": ["major"],
-      "matchBaseBranches": ["stable"],
-      "enabled": false
-    }
-  ]
 };
