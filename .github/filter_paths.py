bd6cae0cc9c63cf30aa68e281d9430e4fe0bb989
Szymon Richert <szymon.rychu@gmail.com>
2023-11-23 08:27:29 +0100

    Fix containers pipeline

---
+++ .github/filter_paths.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+
+def main():
+    json_changed_files = sys.argv[1]
+    changed_files = json.loads(json_changed_files)
+
+    filtered_files = []
+    for f in changed_files:
+        if f.startswith('apps'):
+          filtered_files.append(f)
+
+    print(f"changed_containers={filtered_files}")
+    print(f"changed_containers={filtered_files}", file=sys.stderr)
+
+if __name__ == '__main__':
+    main()
