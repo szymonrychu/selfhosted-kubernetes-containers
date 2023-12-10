c1047b104ec6233e7f18ff36d7b2993ea4bccae0
Szymon Richert <szymon.rychu@gmail.com>
2023-12-10 21:57:12 +0100

    Update code-server image with entrypointd

--- apps/code-server/entrypoint.sh
+++ apps/code-server/entrypoint.sh
@@ -7,6 +7,7 @@ readonly USER_UID="${USER_UID:=1000}"
 readonly USER_GID="${USER_GID:=1000}"
 readonly USER_HOME="${USER_HOME:="/home/${USER_NAME}"}"
 readonly CODER_PORT="${CODER_PORT:=8080}"
+readonly ENTRYPOINTD="${ENTRYPOINTD:=/etc/codeserver.d/}"
 
 groupadd \
     --gid "${USER_GID}" \
@@ -25,7 +26,18 @@ usermod \
     --groups sudo \
         "${USER_NAME}"
 
-cp -Rf /root/.oh-my-zsh /root/.zshrc "${USER_HOME}/"
-chown -R "${USER_UID}:${USER_GID}" "${USER_HOME}/"
+if [[ ! -e "${USER_HOME}/.oh-my-zsh" ]]; then
+    cp -Rf /root/.oh-my-zsh "${USER_HOME}/"
+    chown -R "${USER_UID}:${USER_GID}" "${USER_HOME}/.oh-my-zsh"
+fi
+
+if [[ ! -e "${USER_HOME}/.zshrc" ]]; then
+    cp -Rf /root/.zshrc "${USER_HOME}/"
+    chown -R "${USER_UID}:${USER_GID}" "${USER_HOME}/.zshrc"
+fi
+
+if [[ -d "${ENTRYPOINTD}" ]]; then
+  find "${ENTRYPOINTD}" -type f -executable -print -exec {} \;
+fi
 
 su - "${USER_NAME}" -- "/usr/bin/code-server" --auth none --bind-addr "0.0.0.0:${CODER_PORT}"
