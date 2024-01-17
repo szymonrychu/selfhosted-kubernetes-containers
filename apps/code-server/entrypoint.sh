646a9c0b08b0c9beca04f21b064fc97c27a17597
Szymon Richert <szymon.rychu@gmail.com>
2024-01-17 19:55:00 +0000

    Fix oh-my-zsh permissions, add pre-commit hook

--- apps/code-server/entrypoint.sh
+++ apps/code-server/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -euo nounset
 
-readonly GROUP_NAME="${GROUP_NAME:=user}"
-readonly USER_NAME="${USER_NAME:=user}"
-readonly USER_UID="${USER_UID:=1000}"
-readonly USER_GID="${USER_GID:=1000}"
-readonly USER_HOME="${USER_HOME:="/home/${USER_NAME}"}"
-readonly CODER_PORT="${CODER_PORT:=8080}"
-readonly ENTRYPOINTD="${ENTRYPOINTD:=/etc/codeserver.d/}"
+readonly GROUP_NAME="${GROUP_NAME:-user}"
+readonly USER_NAME="${USER_NAME:-user}"
+readonly USER_UID="${USER_UID:-1000}"
+readonly USER_GID="${USER_GID:-1000}"
+readonly USER_HOME="${USER_HOME:-"/home/${USER_NAME}"}"
+readonly CODER_PORT="${CODER_PORT:-8080}"
+readonly ENTRYPOINTD="${ENTRYPOINTD:-/etc/codeserver.d/}"
 
 groupadd \
     --gid "${USER_GID}" \
@@ -35,6 +35,7 @@ sed -i -e 's/%sudo	ALL=(ALL:ALL) ALL/%sudo	ALL=(ALL:ALL) NOPASSWD:ALL/g' /etc/su
 if [[ ! -e "${USER_HOME}/.oh-my-zsh" ]]; then
     cp -Rf /root/.oh-my-zsh "${USER_HOME}/"
     chown -R "${USER_UID}:${USER_GID}" "${USER_HOME}/.oh-my-zsh"
+    compaudit | xargs chmod g-w,o-w
 fi
 
 if [[ ! -e "${USER_HOME}/.zshrc" ]]; then
