951bfc96f03fdb27434bceab9d8972fb759c66fd
Szymon Richert <szymon.rychu@gmail.com>
2025-01-23 22:30:01 +0100

    fix: Update code server dockerfile

--- apps/code-server/entrypoint.sh
+++ apps/code-server/entrypoint.sh
@@ -2,14 +2,17 @@
 set -euo nounset
 
 if [[ "${USER_NAME:-user}" != "user" ]]; then
-    usermod -md /home/${USER_NAME} -l ${USER_NAME} user || true
+    usermod -md /home/${USER_NAME} -l ${USER_NAME} user
 fi
+
 
 readonly ENTRYPOINTD="/etc/codeserver.d/"
 if [[ -d "${ENTRYPOINTD}" ]]; then
+    echo "export USER_NAME='${USER_NAME:-user}'" >> /tmp/.env
+    
     for f in `find "${ENTRYPOINTD}" -type f -executable -print`; do
         echo "Running '$f'"
-        su -p - "${USER_NAME:-user}" -- "$f" || true
+        su - "${USER_NAME:-user}" -- /bin/bash -ce ". /tmp/.env; $f"
     done
 fi
 
