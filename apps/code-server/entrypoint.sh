a6f9c107df009d8c6ca2e88118dca4864d81dee5
Szymon Richert <szymon.rychu@gmail.com>
2025-01-23 15:57:15 +0100

    fix: Update code server dockerfile

--- apps/code-server/entrypoint.sh
+++ apps/code-server/entrypoint.sh
@@ -2,12 +2,15 @@
 set -euo nounset
 
 if [[ "${USER_NAME:-user}" != "user" ]]; then
-    usermod -md /home/${USER_NAME} -l ${USER_NAME} user
+    usermod -md /home/${USER_NAME} -l ${USER_NAME} user || true
 fi
 
 readonly ENTRYPOINTD="/etc/codeserver.d/"
 if [[ -d "${ENTRYPOINTD}" ]]; then
-    find "${ENTRYPOINTD}" -type f -executable -print -exec su - "${USER_NAME:-user}" -- bash -ce '{}' \;
+    for f in `find "${ENTRYPOINTD}" -type f -executable -print`; do
+        echo "Running '$f'"
+        su - "${USER_NAME:-user}" -- bash -cex "$f" || true
+    done
 fi
 
 exec su - "${USER_NAME:-user}" -- "/usr/bin/code-server" --auth none --bind-addr "0.0.0.0:${CODER_PORT:-8080}" &
